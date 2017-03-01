2abbc8d858fc686cf30be22ac28327e0fedd6b1b
Xavier Dutreilh <xavier@dutreilh.com>
2017-03-01 21:07:41 +0100

    Switch to standard.js

--- test/environments_test.js
+++ test/environments_test.js
@@ -1,18 +1,16 @@
-'use strict';
+const fs = require('fs')
 
-const fs = require('fs');
-
-function read(filename) {
-  return fs.readFileSync(filename, 'utf8');
+function read (filename) {
+  return fs.readFileSync(filename, 'utf8')
 }
 
 exports.environments = {
   'build': (test) => {
-    const actual = read(`${__dirname}/../tmp/index.html`);
-    const expected = read(`${__dirname}/expected/index.html`);
+    const actual = read(`${__dirname}/../tmp/index.html`)
+    const expected = read(`${__dirname}/expected/index.html`)
 
-    test.equal(actual, expected, 'should manage environment-specific settings');
+    test.equal(actual, expected, 'should manage environment-specific settings')
 
-    test.done();
-  },
-};
+    test.done()
+  }
+}
